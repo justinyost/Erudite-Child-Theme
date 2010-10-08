27f4cb45c60855829fd0e435a6c1b54065c054da
jtyost2 <justin.yost@yostivanich.com>
2010-10-08 03:36:37 +0000

    Added a generic function to load stuff for the head, and adding in loading MyOpenID Delegate Information

--- functions.php
+++ functions.php
@@ -3,16 +3,26 @@
 define('CHILD_TEMPLATE_DIR', dirname( get_bloginfo('stylesheet_url')) );
 
 // Add stylesheets
-function add_stylesheets() {
-	
-	$templatedir = get_bloginfo('template_directory');
-	$stylesheetdir = get_bloginfo('stylesheet_directory');
+function _add_stylesheets() {
 	?>
-	
 	<link rel="stylesheet" type="text/css" href="<?php echo CHILD_TEMPLATE_DIR; ?>/css/erudiate-child.css" />
-	
 	<?php
 }
-add_filter('wp_head', 'add_stylesheets');
 
+//Adds MyOpen ID Information to act as a delegate server
+function _add_my_open_id_information(){
+	?>
+	<link rel="openid.server" href="http://www.myopenid.com/server">
+	<link rel="openid.delegate" href="http://jtyost2.myopenid.com/">
+	<?php
+}
+
+//Add Generic stuff to the head
+function add_to_head(){
+	_add_stylesheets();
+	_add_my_open_id_information();
+}
+
+//Add Filters
+add_filter('wp_head', 'add_to_head');
 ?>
